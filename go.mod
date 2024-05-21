a026c29868289dc9f264bf9151dc1ff1cfff6d94
praniil <pparajuli15@gmail.com>
2024-05-21 22:19:21 +0545

    setup mux routers

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.22.2
 
 require github.com/joho/godotenv v1.5.1
 
-require github.com/davecgh/go-spew v1.1.1
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.8.1
+)
--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"strconv"
 	"sync"
 	"time"
+
 	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -35,19 +39,57 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+
 	go func() {
+		defer wg.Done()
 		t := time.Now()
 		genesisBlock := Block{}
 		genesisBlock = Block{0, t.String(), 0, "", calculateHash(genesisBlock), difficulty, ""}
-		fmt.Println("debug above");
+		fmt.Println("debug above")
 		spew.Dump(genesisBlock)
-		fmt.Println("debug below");
+		fmt.Println("debug below")
 
 		mutex.Lock()
 		BlockChain = append(BlockChain, genesisBlock)
 		mutex.Unlock()
 	}()
-	 
+
+	wg.Wait()
+	log.Fatal(run())
+}
+
+func run() error {
+	mux := makeMuxRouter()
+	http_port := os.Getenv("PORT")
+	log.Println("HTTP server listening on port: ", http_port)
+	s := &http.Server{
+		Addr: ":" + http_port,
+		Handler: mux,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := s.ListenAndServe();
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {}
+func handleWriteBlock(w http.ResponseWriter, r *http.Request)    {
+	w.Header().Set("Content-Type", "application/json")
 	
 }
 
